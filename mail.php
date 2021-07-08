15e8579e21e21129865531f1b3cd98a76ba94313
jenya1995 <65701920+jenya1995@users.noreply.github.com>
2021-07-08 13:52:27 +0300

    next

--- mail.php
+++ mail.php
@@ -13,7 +13,7 @@ $product = $_POST['user_product'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.yandex.ru';  					// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'vitali.milohin@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'vitali.milohin652325'; // Ваш пароль от почты с которой будут отправляться письма
@@ -22,12 +22,7 @@ $mail->Port = 465; // TCP port to connect to / этот порт может от
 
 $mail->setFrom('vitali.milohin@yandex.ru'); // от кого будет уходить письмо?
 $mail->addAddress('artem.furoff@yandex.ru');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Заявка с сайта';
